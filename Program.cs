47155d73484695a8d0039154c816045f75054b98
3109724 <3109724@TE-GDA-D-D12C31>
2022-03-25 11:43:29 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Szyfr_Cezara
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var c in Console.ReadLine())
+            {
+                Console.Write(Convert.ToChar((c - 'a' + 1) % ('z' - 'a' + 1) + 'a'));
+            }
+            Console.WriteLine();
+            Console.ReadKey(true);
+        }
+    }
+}
